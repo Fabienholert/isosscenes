14b38034af1dd317c4754666596f2b68b19fbb80
Fabienholert <fabienholert@gmail.com>
2025-03-30 23:35:17 +0200

    Ajout des liens de navigation dans l'en-tête

--- src/components/header/page.tsx
+++ src/components/header/page.tsx
@@ -1,8 +1,16 @@
+import { Link } from "react-router-dom";
 import Logo from "../../../public/logo.png";
+
 export default function Header() {
   return (
     <header>
-      <nav></nav>
+      <nav>
+        <Link to="/"> Accueil </Link>
+        <Link to="/photos">Chronique photos</Link>
+        <Link to="/photosReportage">Reportage Photos</Link>
+        <Link to="/albums">Chronique Album</Link>
+        <Link to="/interviews">Interviews</Link>
+      </nav>
       <img src={Logo} alt="logo IsosScenes" />
     </header>
   );
