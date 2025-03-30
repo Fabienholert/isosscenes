1afa6e9c735d8332fc7bcf72d7de2af22f12c06d
Fabienholert <fabienholert@gmail.com>
2025-03-30 23:38:54 +0200

    Ajout de nouvelles routes et liens pour les sections photos, albums et interviews

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,16 @@ export default function App() {
         <Routes>
           <Route element={<Layout />}>
             <Route path="/" element={<Home />} />
+            <Route path="/photos" element={<Home />} />
+            <Route path="/photosReportage" element={<Home />} />
+            <Route path="/albums" element={<Home />} />
+            <Route path="/interviews" element={<Home />} />
+            <Route path="/connexion" element={<Home />} />
+            <Route path="/membres" element={<Home />} />
+            <Link to="/photos">Chronique photos</Link>
+            <Link to="/photosReportage">Reportage Photos</Link>
+            <Link to="/albums">Chronique Album</Link>
+            <Link to="/interviews">Interviews</Link>
           </Route>
         </Routes>
       </BrowserRouter>
