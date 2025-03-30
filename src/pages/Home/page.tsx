f9d4cd8dc8c17991c1788c385b297e48a372c74f
Fabienholert <fabienholert@gmail.com>
2025-03-30 23:14:10 +0200

    Mise à jour de la page d'accueil pour afficher les dernières publications et les prochaines sorties

--- src/pages/Home/page.tsx
+++ src/pages/Home/page.tsx
@@ -1,3 +1,12 @@
 export default function Home() {
-  return <div>test</div>;
+  return (
+    <>
+      <div>
+        <h2>Nos dernières publications</h2>
+      </div>
+      <div>
+        <h2>Nos prochaines sorties</h2>
+      </div>
+    </>
+  );
 }
