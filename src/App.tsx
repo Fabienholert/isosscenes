33cb26f6a2ff4203da28b609313d50b1ae9c7f78
Fabienholert <fabienholert@gmail.com>
2025-03-30 23:48:14 +0200

    Ajout des routes et des composants pour les pages Albums, Connexion, Membres, Interviews, et Photos

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,12 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Layout from "./layout.js";
+import Albums from "./pages/albums/page.js";
+import Connexion from "./pages/connexion/page.js";
 import Home from "./pages/Home/page.js";
+import Interviews from "./pages/interviews/page.js";
+import Membres from "./pages/membre/page.js";
+import Photos from "./pages/photosChronique/page.js";
+import PhotosReport from "./pages/photosReportage/page.js";
 export default function App() {
   return (
     <div id="accueil">
@@ -8,16 +14,12 @@ export default function App() {
         <Routes>
           <Route element={<Layout />}>
             <Route path="/" element={<Home />} />
-            <Route path="/photos" element={<Home />} />
-            <Route path="/photosReportage" element={<Home />} />
-            <Route path="/albums" element={<Home />} />
-            <Route path="/interviews" element={<Home />} />
-            <Route path="/connexion" element={<Home />} />
-            <Route path="/membres" element={<Home />} />
-            <Link to="/photos">Chronique photos</Link>
-            <Link to="/photosReportage">Reportage Photos</Link>
-            <Link to="/albums">Chronique Album</Link>
-            <Link to="/interviews">Interviews</Link>
+            <Route path="/photos" element={<Photos />} />
+            <Route path="/photosReportage" element={<PhotosReport />} />
+            <Route path="/albums" element={<Albums />} />
+            <Route path="/interviews" element={<Interviews />} />
+            <Route path="/connexion" element={<Connexion />} />
+            <Route path="/membres" element={<Membres />} />
           </Route>
         </Routes>
       </BrowserRouter>
