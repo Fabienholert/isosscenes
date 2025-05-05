d023b22e1ba3ffcb414f27cd7d0cffb56562000d
Fabienholert <fabienholert@gmail.com>
2025-05-05 13:30:47 +0200

    Ajout du formulaire de connexion avec les champs email et mot de passe

--- src/pages/connexion/page.tsx
+++ src/pages/connexion/page.tsx
@@ -1,7 +1,21 @@
 export default function Connexion() {
   return (
     <div>
-      <form></form>
+      <form>
+        <label htmlFor="email">Email:</label>
+        <input type="email" id="email" name="email" required />
+        <br />
+        <label htmlFor="password">Mot de passe:</label>
+        <input type="password" id="password" name="password" required />
+        <br />
+        <button type="submit">Se connecter</button>
+        <button type="button" onClick={() => alert("Mot de passe oublié?")}>
+          Mot de passe oublié?
+        </button>
+        <button type="button" onClick={() => alert("Créer un compte")}>
+          Créer un compte
+        </button>
+      </form>
     </div>
   );
 }
